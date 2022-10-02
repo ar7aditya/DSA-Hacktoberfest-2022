a5be31a2760e37582b244f2b0cae9ecd2e0e4a5e
ajruttam <ajruttam@gmail.com>
2022-10-02 08:42:15 +0530

    Added sample input/output

--- PYTHON/matrix_multiply.py
+++ PYTHON/matrix_multiply.py
@@ -3,6 +3,32 @@ Multiplication of 2 - matrices
 
 Sample I/O:
 
+Example 1:
+Enter the dimensions of Matrix 1(separated by a space): 3 3
+Enter the elements of Matrix 1
+1 1 1
+2 2 2 
+3 3 3
+Enter the dimensions of Matrix 2(separated by a space): 3 4
+Enter the elements of Matrix 2
+1 1 1 1
+2 2 2 2
+3 3 3 3
+Multiplication of Matrix 1 and Matrix 2 is: 
+6 6 6 6
+12 12 12 12
+18 18 18 18
+
+Example 2:
+Enter the dimensions of Matrix 1(separated by a space): 3 2
+Enter the elements of Matrix 1
+1 2
+3 4
+5 6
+Enter the dimensions of Matrix 2(separated by a space): 1 5
+Enter the elements of Matrix 2
+9 8 7 4 6
+Matrices cannot be multiplied
 """
 
 r1,c1 = map(int,input("Enter the dimensions of Matrix 1(separated by a space): ").split())
@@ -26,10 +52,9 @@ else:
         for j in range(c2):
             # iterating by row of Matrix m2
             for k in range(r2):
-                m3[i][j] += m1[i][k] * m2[k][j]
-            
+                m3[i][j] += m1[i][k] * m2[k][j]     
 
-# printing the resultant matrix
-print("Multiplication of Matrix 1 and Matrix 2 is: ")
-for row in m3:
-    print(*row)
+    # printing the resultant matrix
+    print("Multiplication of Matrix 1 and Matrix 2 is: ")
+    for row in m3:
+        print(*row)
