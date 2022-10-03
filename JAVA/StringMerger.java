e4ef21b45bfc20eae7499deb83f468b5b6fe8c8b
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-10-03 23:16:04 +0530

    Update StringMerger.java

--- JAVA/StringMerger.java
+++ JAVA/StringMerger.java
@@ -1,3 +1,9 @@
+/*At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
+
+The restriction is that the characters in part1 and part2 should be in the same order as in s.
+
+The interviewer gives you the following example and tells you to figure out the rest from the given test cases. */
+
 public class StringMerger {
     public static boolean isMerge(String s, String part1, String part2){
         if (s.length() != part1.length() + part2.length()) return false;
