f9646e7fbb62f24da6a7b7fa2ee5934d3e2a743e
rithwik003 <amaraneni.rithwik@gmail.com>
2022-10-04 15:21:48 +0530

    Added equilibrium_point.cpp
    Signed-off-by: rithwik003 <amaraneni.rithwik@gmail.com>

---
+++ C++/equilibrium_point.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+        int left=0,right=0;
+        for(int i=0;i<n;i++){
+            right+=a[i];
+        }
+        for(int i=0;i<n;i++){
+            right-=a[i];
+            if(right==left){
+                return i+1;
+            }
+            else{
+                left+=a[i];
+            }
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
